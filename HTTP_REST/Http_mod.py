5debe1b489f47a90739d53db3234e6e7b1bd5b0c
Kaveri Priya <kaveri.priya@revature.com>
2023-06-23 12:04:27 +0530

    Deleted

--- HTTP_REST/Http_mod.py
+++
@@ -1,9 +0,0 @@
-import http.client
-req=http.client.HTTPConnection('www.google.com',80,timeout=10)
-req.request("GET","/")
-res=req.getresponse()
-gethead=res.getheaders()
-print(gethead)
-print(res.reason)
-print(res.status)
-req.close()
--- Rest_DB_Flask/src/Student.py
+++
@@ -1,4 +0,0 @@
-class Student:
-    def __init__(self, name, roll_number):
-        self.name = name
-        self.roll_number = roll_number
